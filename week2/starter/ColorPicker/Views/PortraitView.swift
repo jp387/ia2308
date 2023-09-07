a754af8309f14c6eef0701775dff21eecff2e4a1
jp387 <141362328+jp387@users.noreply.github.com>
2023-09-07 12:52:14 -0700

    Created the Portrait View file to consolidate the views in an attempt to solve the Above and Beyond challenge

--- week2/starter/ColorPicker/Views/PortraitView.swift
+++ week2/starter/ColorPicker/Views/PortraitView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,31 +32,35 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
-  @State private var redColor: Double = 250.0
-  @State private var greenColor: Double = 100.0
-  @State private var blueColor: Double = 50.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-  
-  init() {
-    _foregroundColor = State(initialValue: Color(red: redColor/255, green: greenColor/255, blue: blueColor/255))
-  }
+struct PortraitView: View {
+  @Binding var redColor: Double
+  @Binding var greenColor: Double
+  @Binding var blueColor: Double
+  @Binding var foregroundColor: Color
   
   var body: some View {
-    PortraitView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, foregroundColor: $foregroundColor)
+    VStack {
+      TitleTextViews(text: "Color Picker")
+      RoundedRectViews(foregroundColor: $foregroundColor)
+      VStack {
+        SliderView(color: $redColor, text: "Red")
+          .tint(.red)
+        SliderView(color: $greenColor, text: "Green")
+          .tint(.green)
+        SliderView(color: $blueColor, text: "Blue")
+        ButtonViews(foregroundColor: $foregroundColor, redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, text: "Set Color")
+      }
+      .padding()
+    }
+    .background(Color("BackgroundColor"))
+    .padding(20)
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct PortraitView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
-    ContentView()
+    PortraitView(redColor: .constant(250), greenColor: .constant(100), blueColor: .constant(50), foregroundColor: .constant(Color.yellow))
+    PortraitView(redColor: .constant(250), greenColor: .constant(100), blueColor: .constant(50), foregroundColor: .constant(Color.yellow))
       .preferredColorScheme(.dark)
-    ContentView()
-      .previewInterfaceOrientation(.landscapeRight)
-    ContentView()
-      .preferredColorScheme(.dark)
-      .previewInterfaceOrientation(.landscapeRight)
   }
 }
